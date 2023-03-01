6d4fa200dd09f7b1e9fb801392076709ada6853a
ChristopherChantres <chris.chantres@gmail.com>
2023-03-01 02:35:57 -0500

    UserImageLink is being passed to /analyze with GET Method.

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-  return HttpResponse('<p>The URL connnection is done!</p>')
+  context = {
+    'name': 'Anonymous',
+  }
+
+  return render(request, 'index.html', context)
+
+
+# gets the ulr image from the input and passes to the analyze.html file
+def analyze(request):
+  context = {
+    'user_url': request.GET['userUrlPhoto']
+  }
+  
+  return render(request, 'analyze.html', context)
