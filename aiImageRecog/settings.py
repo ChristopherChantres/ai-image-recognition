3e546ebae5822f96374d2b35894b58c820bd0699
ChristopherChantres <chris.chantres@gmail.com>
2023-03-01 01:44:52 -0500

    Templates file created and updated to settings.py

--- aiImageRecog/settings.py
+++ aiImageRecog/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'aiImageRecog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
